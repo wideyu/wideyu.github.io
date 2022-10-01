e84e47fe07700e5a8f18e165dc4d9948b9a15885
wideyu <wideyu@gmail.com>
2022-10-01 13:15:19 +0800

    Create 编辑.sql

---
+++ 编辑.sql
@@ -0,0 +1,260 @@
+SET autocommit=0
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+commit
+SET autocommit=1
+SET autocommit=0
+"SELECT  id,C001005 AS postName,C001001 AS postClass,C001201 AS postType,C001015 AS superId,C001003 AS postCodeNumber,C001204 AS isPostkey,C001200 AS actualPerson,C001203 AS postLevel,C001735 AS postManger,C001210 AS lackPerson,C001209 AS outPerson,C001701 AS orgTreeId,C001010 AS orgId,C001222 AS postDeleteFlag,C001214 AS keyYear,C001208 AS personCount,C001700 AS standardPost  FROM C001 
+ 
+ WHERE (id IN ('6bdcbaf31974d1947fa1938bb30d4cac'))"
+commit
+SET autocommit=1
+SET autocommit=0
+"SELECT  id AS orgId,B001002 AS superId,B001003 AS treeId,b001005 AS name,B001010 AS orgCode,B001050 AS orgLevel,B001201 AS orgEstablishment,B001202 AS socialCreditCode,B001206 AS attributionLine,B001209 AS attachFile,B001210 AS orgAttribute,B001211 AS orgType,B001220 AS organizationCode,B001240 AS postCode,B001245 AS registeredAddress,B001249 AS telephone,B001255 AS orgManager,B001260 AS financialLicense,B001261 AS areaNum,B001715 AS orgSort,B001730 AS isDelete,B001227 AS establishTime,B001203 AS actualPerson,B001205 AS lackPerson,B001204 AS outPerson,B001701 AS strongControlFlag  FROM B001 
+ 
+ WHERE (id IN ('101')) ORDER BY B001715 ASC"
+commit
+SET autocommit=1
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+SET autocommit=0
+"select
+         
+         
+            *
+         
+        from A001
+         WHERE ID='1' 
+         
+            order by 2"
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+"SELECT  id,C001005 AS postName,C001001 AS postClass,C001201 AS postType,C001015 AS superId,C001003 AS postCodeNumber,C001204 AS isPostkey,C001200 AS actualPerson,C001203 AS postLevel,C001735 AS postManger,C001210 AS lackPerson,C001209 AS outPerson,C001701 AS orgTreeId,C001010 AS orgId,C001222 AS postDeleteFlag,C001214 AS keyYear,C001208 AS personCount,C001700 AS standardPost  FROM C001 
+ 
+ WHERE (id IN ('6bdcbaf31974d1947fa1938bb30d4cac'))"
+"SELECT  id AS orgId,B001002 AS superId,B001003 AS treeId,b001005 AS name,B001010 AS orgCode,B001050 AS orgLevel,B001201 AS orgEstablishment,B001202 AS socialCreditCode,B001206 AS attributionLine,B001209 AS attachFile,B001210 AS orgAttribute,B001211 AS orgType,B001220 AS organizationCode,B001240 AS postCode,B001245 AS registeredAddress,B001249 AS telephone,B001255 AS orgManager,B001260 AS financialLicense,B001261 AS areaNum,B001715 AS orgSort,B001730 AS isDelete,B001227 AS establishTime,B001203 AS actualPerson,B001205 AS lackPerson,B001204 AS outPerson,B001701 AS strongControlFlag  FROM B001 
+ 
+ WHERE (id IN ('101')) ORDER BY B001715 ASC"
+commit
+SET autocommit=1
+SET autocommit=0
+"select
+         
+         
+            *
+         
+        from A001
+         WHERE ID='1'"
+"SELECT COUNT( * ) FROM A001 
+ 
+ WHERE (A001735 = '0超级管理员' AND ID <> '1')"
+SELECT @@session.transaction_read_only
+"update A001 set
+          
+            a001077='510107195106117892'
+         , 
+            a001011='1951-06-11'
+         , 
+            a001254='2004-08-01'
+         , 
+            a001232='3048000643'
+         , 
+            a001750='0188000001'
+         , 
+            a001730='00900'
+         , 
+            a001213='hello'
+         , 
+            a001214='2021-12-25'
+         , 
+            a001735='0超级管理员'
+         , 
+            a001218='2009-07-01'
+         , 
+            a001715='6bdcbaf31974d1947fa1938bb30d4cac'
+         , 
+            a001738='1001'
+         , 
+            a001718='2018-08-14'
+         , 
+            id='1'
+         , 
+            a001041='2004-08-01'
+         , 
+            a001285=null
+         , 
+            a001044='2004-08-01'
+         , 
+            a001220='2004-08-01'
+         , 
+            a001001='超级管理员'
+         , 
+            a001287='01302'
+         , 
+            a001245=null
+         , 
+            a001201='0120000001'
+         , 
+            a001223='admin.it@ciic.com.cn'
+         , 
+            a001225='1493406485392719873,2.jpg'
+         , 
+            a001203=null
+         , 
+            a001247=null
+         , 
+            a001226=null
+         , 
+            a001248='011502'
+         , 
+            a001743='1001'
+         , 
+            a001007='01001'
+         , 
+            a001205='70'
+         , 
+            a001700=null
+         , 
+            a001228='3133100225'
+         , 
+            a001701='101'
+         , 
+            a001745='12'
+         , 
+            a001702='gd'
+         , 
+            a001725='0145100301'
+         , 
+            a001208='0105110000'
+         , 
+            a001209='现居住地址21091303'
+         , 
+            a001705='101'
+         , 
+            a001728='1001'
+         , 
+            a001270=null
+         , 
+            a001271='18812345678'
+         , 
+            a001054='013511'
+         
+         WHERE 1=1
+            
+               and ID='1'"
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+SELECT @@session.transaction_read_only
+"update A001
+         SET create_time='2022-10-01 12:14:09',
+            last_update_time='2022-10-01 12:14:09',
+            LAST_OPERATOR='1' 
+         WHERE 1=1
+            
+                and(ID in
+                (
+                    '1'
+                ))"
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+SELECT @@session.transaction_read_only
+"INSERT INTO sys_system_log  ( id,
+user_id,
+data_id,
+opert_type,
+old_data,
+new_data,
+opert_time,
+table_name,
+filed_name,
+pk_id,
+fk_id,
+set_id,
+item_id,
+old_data_source,
+new_data_source )  VALUES  ( '1576062891796770818',
+'1',
+'超级管理员',
+'修改',
+'1493406485392719873,2.jpg',
+'1493406485392719873',
+'2022-10-01 12:14:09',
+'员工基本信息',
+'员工照片',
+'1',
+'1',
+'A001',
+'A001225',
+'1493406485392719873,2.jpg',
+'1493406485392719873' )"
+"INSERT INTO sys_system_log  ( id,
+user_id,
+data_id,
+opert_type,
+old_data,
+new_data,
+opert_time,
+table_name,
+filed_name,
+pk_id,
+fk_id,
+set_id,
+item_id,
+old_data_source,
+new_data_source )  VALUES  ( '1576062891817742337',
+'1',
+'超级管理员',
+'修改',
+'test',
+'hello',
+'2022-10-01 12:14:09',
+'员工基本信息',
+'备注',
+'1',
+'1',
+'A001',
+'A001213',
+'test',
+'hello' )"
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+"SELECT ID,A001001 AS name,A001007 AS sex,A001011 AS birthday,A001041 AS joinWorkDate,A001044 AS joinSysDate,A001054 AS useType,A001077 AS idNum,A001201 AS partyFigure,A001225 AS photo,A001701 AS orgId,A001705 AS deptId,A001715 AS postId,A001718 AS partiesTime,A001725 AS perState,A001728 AS orgTreeId,A001735 AS personCode,A001730 AS cancel,A001738 AS deptTreeId,A001743 AS deptSort,A001745 AS perSort,A001750 AS cadreType,A001203 AS duty,A001226 AS usedName,A001245 AS postRank,A001254 AS nowPostTime,A001205 AS age,A001208 AS pesidentPlace,A001209 AS nowHomePlace,A001218 AS nowJobTime,A001220 AS nowRankTime,A001700 AS financialWorkTime,A001228 AS idType,A001232 AS nationality,A001214 AS inCompanyTime,A001248 AS nation,A001213 AS remarks,A001247 AS confirmationDate,A001223 AS email,A001270 AS wxOpenId,A001271 AS mobile,A001285 AS nativePlace,A001287 AS maritalStatus FROM A001 WHERE ID='1'"
+SELECT @@session.transaction_read_only
+"UPDATE A001  SET A001001='超级管理员',
+A001007='01001',
+A001011='1951-06-11',
+A001041='2004-08-01',
+A001044='2004-08-01',
+A001054='013511',
+A001077='510107195106117892',
+A001201='0120000001',
+A001225='1493406485392719873,2.jpg',
+A001701='101',
+A001705='101',
+A001715='6bdcbaf31974d1947fa1938bb30d4cac',
+A001718='2018-08-14',
+A001725='0145100301',
+A001728='1001',
+A001735='0超级管理员',
+A001730='00900',
+A001738='1001',
+A001743='1001',
+A001745='12',
+A001750='0188000001',
+
+
+
+A001254='2004-08-01',
+A001205='70',
+A001208='0105110000',
+A001209='现居住地址21091303',
+A001218='2009-07-01',
+A001220='2004-08-01',
+
+A001228='3133100225',
+A001232='3048000643',
+A001214='2021-12-25',
+A001248='011502',
+A001213='hello',
+
+A001223='admin.it@ciic.com.cn',
+
+A001271='18812345678',
+
+A001287='01302'  WHERE ID='1'"
+commit
+SET autocommit=1
